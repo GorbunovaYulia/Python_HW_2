1c8736ac67123dfc2a0a90c2f97b45c1c8607904
YuliaGorbunova <yuliagorbunova@yahoo.com>
2023-03-24 22:57:35 +0500

    HW_10

---
+++ EX_2.py
@@ -0,0 +1,8 @@
+#ѕровер€ет €вл€етс€ ли число квадратом другого
+number_1 = int(input())
+number_2 = int(input())
+#if number_1**2 == number_2:
+#    print('Yes')
+#else:
+#    print('No')
+print(number_1**2 == number_2 or number_2**2 == number_1)
---
+++ Ex_3.py
@@ -0,0 +1,5 @@
+#Выводит числа от -N до N
+
+n = int(input("x="))
+for i in range(-n,n+1):
+    print(i, end=" ")
